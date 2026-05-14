937fece6ed32b57ed4fb53504b74f35ecdddca3a
Ivan <116540422+Grosh16@users.noreply.github.com>
2026-05-14 04:38:24 +0400

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -131,7 +131,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 async function getRequestAll() {
     try {
-        const res = await fetch(`https://kval-backend.onrender.com/request/all`, {
+        const res = await fetch(`https://kvalback.onrender.comrequest/all`, {
             method: 'GET',
         });
 
@@ -223,7 +223,7 @@ async function updateStatus() {
     }
 
     try {
-        const res = await fetch('https://kval-backend.onrender.com/request/status', {
+        const res = await fetch('https://kvalback.onrender.comrequest/status', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
